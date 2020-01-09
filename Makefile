08d5bccdffb4843a56ac5c09d49a9208f9aec7d7
Dan Gastler <dgastler@gmail.com>
2020-01-09 11:58:47 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,93 @@
+CXX?=g++
+
+INCLUDE_PATH = \
+		-Iinclude  
+
+LIBRARY_PATH = \
+		-Llib 
+
+
+ifdef BOOST_INC
+INCLUDE_PATH +=-I$(BOOST_INC)
+endif
+ifdef BOOST_LIB
+LIBRARY_PATH +=-L$(BOOST_LIB)
+endif
+
+LIBRARIES =    	-lboost_regex \
+		-lboost_filesystem
+
+
+#CXX_FLAGS = -std=c++11 -g -O3 -rdynamic -Wall -MMD -MP -fPIC ${INCLUDE_PATH} -Werror -Wno-literal-suffix 
+CXX_FLAGS = -std=c++11 -g -O3 -rdynamic -Wall -MMD -MP -fPIC ${INCLUDE_PATH} -Wno-literal-suffix 
+
+CXX_FLAGS +=-fno-omit-frame-pointer -Wno-ignored-qualifiers -Werror=return-type -Wextra -Wno-long-long -Winit-self -Wno-unused-local-typedefs  -Woverloaded-virtual ${COMPILETIME_ROOT} ${FALLTHROUGH_FLAGS}
+
+LINK_LIBRARY_FLAGS = -shared -fPIC -Wall -g -O3 -rdynamic ${LIBRARY_PATH} ${LIBRARIES} -Wl,-rpath=$(RUNTIME_LDPATH)/lib ${COMPILETIME_ROOT}
+
+
+# ------------------------
+# IPBUS stuff
+# ------------------------
+UHAL_LIBRARIES = -lcactus_uhal_log 		\
+                 -lcactus_uhal_grammars 	\
+                 -lcactus_uhal_uhal 		
+
+# Search uHAL library from $IPBUS_PATH first then from $CACTUS_ROOT
+ifdef IPBUS_PATH
+UHAL_INCLUDE_PATH = \
+			-isystem$(IPBUS_PATH)/uhal/uhal/include \
+	         	-isystem$(IPBUS_PATH)/uhal/log/include \
+	         	-isystem$(IPBUS_PATH)/uhal/grammars/include 
+UHAL_LIBRARY_PATH = \
+			-L$(IPBUS_PATH)/uhal/uhal/lib \
+	         	-L$(IPBUS_PATH)/uhal/log/lib \
+	         	-L$(IPBUS_PATH)/uhal/grammars/lib \
+			-L$(IPBUS_PATH)/extern/pugixml/pugixml-1.2/ 
+else
+UHAL_INCLUDE_PATH = \
+	         	-isystem$(CACTUS_ROOT)/include 
+
+UHAL_LIBRARY_PATH = \
+			-L$(CACTUS_ROOT)/lib  -Wl,-rpath=$(CACTUS_ROOT)/lib
+endif
+
+UHAL_CXX_FLAGS = ${UHAL_INCLUDE_PATH}
+CXX_FLAGS+=${UHAL_CXX_FLAGS}
+
+UHAL_LIBRARY_FLAGS = ${UHAL_LIBRARY_PATH}
+LINK_LIBRARY_FLAGS+=${UHAL_LIBRARY_FLAGS}
+
+.PHONY: all _all clean _cleanall build _buildall _cactus_env
+
+default: build
+clean: _cleanall
+_cleanall:
+	rm -rf obj
+	rm -rf lib
+
+
+all: _all
+build: _all
+buildall: _all
+_all: _cactus_env lib/libUIOuHAL.so
+
+
+_cactus_env:
+ifdef IPBUS_PATH
+	$(info using uHAL lib from user defined IPBUS_PATH=${IPBUS_PATH})
+else ifdef CACTUS_ROOT
+	$(info using uHAL lib from user defined CACTUS_ROOT=${CACTUS_ROOT})
+else
+	$(error Must define IPBUS_PATH or CACTUS_ROOT to include uHAL libraries (define through Makefile or command line\))
+endif
+
+
+
+lib/libUIOuHAL.so : obj/ProtocolUIO.o 
+	mkdir -p lib
+	${CXX} ${LINK_LIBRARY_FLAGS}  $^ -o $@
+
+obj/%.o : src/%.cpp
+	mkdir -p obj
+	${CXX} ${CXX_FLAGS} $^ -o $@
---
+++ include/ProtocolUIO.hpp
@@ -0,0 +1,127 @@
+/*
+  ---------------------------------------------------------------------------
+
+  This is an extension of uHAL to directly access AXI slaves via the linux
+  UIO driver. 
+
+  This file is part of uHAL.
+
+  uHAL is a hardware access library and programming framework
+  originally developed for upgrades of the Level-1 trigger of the CMS
+  experiment at CERN.
+
+  uHAL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  uHAL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with uHAL.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  Andrew Rose, Imperial College, London
+  email: awr01 <AT> imperial.ac.uk
+
+  Marc Magrans de Abril, CERN
+  email: marc.magrans.de.abril <AT> cern.ch
+
+  Tom Williams, Rutherford Appleton Laboratory, Oxfordshire
+  email: tom.williams <AT> cern.ch
+
+  Dan Gastler, Boston University 
+  email: dgastler <AT> bu.edu
+      
+  ---------------------------------------------------------------------------
+*/
+/**
+   @file
+   @author Siqi Yuan / Dan Gastler / Theron Jasper Tarigo
+*/
+
+#ifndef _uiouhal_ProtocolUIO_hpp_
+#define _uiouhal_ProtocolUIO_hpp_
+
+#include <uhal/ClientInterface.hpp>
+#include <uhal/ValMem.hpp>
+#include "uhal/log/exception.hpp"
+#include <signal.h> //for handling of SIG_BUS signals
+
+namespace uioaxi {
+
+  //should only change the ADDR_DEV_BITS
+  const int ADDR_DEV_BITS = 8;
+  const int ADDR_DEV_OFFSET = 32-ADDR_DEV_BITS;
+  const uint32_t ADDR_WORD_MASK = (0x1 << ADDR_DEV_OFFSET) - 1;
+  const uint32_t ADDR_DEV_MASK  = ~ADDR_WORD_MASK;
+  const int DEVICES_MAX = 0x1 << ADDR_DEV_BITS;
+  const int DEVNUMPRLEN = 1+(ADDR_DEV_BITS/4-1);
+
+  typedef uint8_t devnum_t;
+  typedef uint16_t wordnum_t;
+
+  struct DevAddr {
+    devnum_t device;
+    wordnum_t word;
+  };
+
+}
+
+namespace uhal {
+
+  namespace exception
+  {
+    UHAL_DEFINE_EXCEPTION_CLASS ( UnmatchedLabel , "Exception class to handle the case where matching a label to a device failed." )
+    UHAL_DEFINE_EXCEPTION_CLASS ( BadUIODevice , "Exception class to handle the case where uio device cannot be opened." )
+    UHAL_DEFINE_EXCEPTION_CLASS ( UnimplementedFunction , "Exception class to handle the case where an unimplemented function is called." )
+    UHAL_DEFINE_EXCEPTION_CLASS ( UIOBusError , "Exception class for when an axi transaction causes a BUS_ERROR." )
+  }
+
+  class UIO : public ClientInterface {
+  public:
+    UIO (
+	 const std::string& aId, const URI& aUri,
+	 const boost::posix_time::time_duration&aTimeoutPeriod =
+	 boost::posix_time::milliseconds(10)
+	 );
+    virtual ~UIO ();
+  protected:
+    ValHeader implementWrite (const uint32_t& aAddr, const uint32_t& aValue);
+    ValWord<uint32_t> implementRead (const uint32_t& aAddr,
+				     const uint32_t& aMask = defs::NOMASK);
+    void implementDispatch (boost::shared_ptr<Buffers> aBuffers) /*override*/ ;
+
+    ValHeader implementBOT();
+    ValHeader implementWriteBlock (const uint32_t& aAddr, const std::vector<uint32_t>& aValues, const defs::BlockReadWriteMode& aMode=defs::INCREMENTAL);
+    ValVector< uint32_t > implementReadBlock ( const uint32_t& aAddr, const uint32_t& aSize, const defs::BlockReadWriteMode& aMode=defs::INCREMENTAL );
+    ValWord< uint32_t > implementRMWbits ( const uint32_t& aAddr , const uint32_t& aANDterm , const uint32_t& aORterm );
+    ValWord< uint32_t > implementRMWsum ( const uint32_t& aAddr , const int32_t& aAddend );
+    uint32_t getMaxNumberOfBuffers() {return 0;}
+    uint32_t getMaxSendSize() {return 0;}
+    uint32_t getMaxReplySize() {return 0;}
+
+    exception::exception* validate ( uint8_t* aSendBufferStart ,
+				     uint8_t* aSendBufferEnd ,
+				     std::deque< std::pair< uint8_t* , uint32_t > >::iterator aReplyStartIt ,
+				     std::deque< std::pair< uint8_t* , uint32_t > >::iterator aReplyEndIt ) {
+      return NULL;}
+  private:
+    int fd[uioaxi::DEVICES_MAX];
+    uint32_t volatile * hw[uioaxi::DEVICES_MAX];
+    uint32_t addrs[uioaxi::DEVICES_MAX];
+    char uionames[uioaxi::DEVICES_MAX][128];
+    uioaxi::DevAddr decodeAddress (uint32_t uaddr);
+    std::vector< ValWord<uint32_t> > valwords;
+    void primeDispatch ();
+    void openDevice (int devnum, uint32_t size, const char *name);
+    int checkDevice (int devnum);
+    struct sigaction saBusError;
+    struct sigaction saBusError_old;
+  };
+
+}
+#endif
