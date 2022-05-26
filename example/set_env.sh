6b7642331a91d2ed7b6560f8f77109601649ea5d
Alp <45109652+alpakpinar@users.noreply.github.com>
2022-05-26 12:30:29 -0400

    Set UHAL_ENABLE_IPBUS_MMAP to enable mmap protocol

--- example/set_env.sh
+++ example/set_env.sh
@@ -9,3 +9,6 @@ UIOUHAL_ROOT="/opt/UIOuHAL"
 LD_LIBRARY_PATH=${UIOUHAL_ROOT}/lib:${LD_LIBRARY_PATH}
 
 echo "Set the LD_LIBRARY_PATH to: ${LD_LIBRARY_PATH}"
+
+# Set environment variable to enable MMAP protocol
+export UHAL_ENABLE_IPBUS_MMAP=1
